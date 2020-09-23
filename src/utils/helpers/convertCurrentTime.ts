e7dbd548926cead0925e889bc7f5f4fc1b49021d
andrew <corvusalbus829@gmail.com>
2020-09-23 21:38:51 +0300

    change convertCurrentTime

--- src/utils/helpers/convertCurrentTime.ts
+++ src/utils/helpers/convertCurrentTime.ts
@@ -2,14 +2,14 @@ const convertCurrentTime = (number: number): string => {
   const hours = Math.floor(number / 3600);
   const mins = Math.floor(number / 60) - (hours * 60);
   const secs = parseInt((number % 60).toFixed(), 10);
-  if (hours > 0) {
-  return `${hours < 10 ? '0' : ''}${hours}:${mins < 10 ? '0' : ''}${mins}:${secs < 10 ? '0' : ''}${secs}`;
-  }
-  if (mins > 0) {
-    return `${mins}:${secs}`;
-  }
-  return `${secs}`;
+  // if (hours > 0) {
   // return `${hours < 10 ? '0' : ''}${hours}:${mins < 10 ? '0' : ''}${mins}:${secs < 10 ? '0' : ''}${secs}`;
+  // }
+  // if (mins > 0) {
+  //   return `${mins}:${secs}`;
+  // }
+  // return `${secs}`;
+  return `${hours < 10 ? '0' : ''}${hours}:${mins < 10 ? '0' : ''}${mins}:${secs < 10 ? '0' : ''}${secs}`;
 };
 
 export default convertCurrentTime;
